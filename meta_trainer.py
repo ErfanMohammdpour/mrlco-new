9fac1a1e4a7b90b19f4333ac4d5bb27e0260d757
Erfan Mhp <thisiserfun@gmail.com>
2025-07-28 15:27:49 +0330

    fix bug

--- meta_trainer.py
+++ meta_trainer.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
                         sampler=sampler,
                         sample_processor=sample_processor,
                         policy=meta_policy,
-                        n_itr=500,
+                        n_itr=300,
                         greedy_finish_time= greedy_finish_time,
                         start_itr=0,
                         inner_batch_size=1000)
