c67ac7577ef86c5644bbb313db0b84aace097874
Erfan Mhp <thisiserfun@gmail.com>
2025-07-31 14:40:14 +0330

    64 -> 32

--- meta_trainer.py
+++ meta_trainer.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
                         sampler=sampler,
                         sample_processor=sample_processor,
                         policy=meta_policy,
-                        n_itr=500,
+                        n_itr=1500,
                         greedy_finish_time= greedy_finish_time,
                         start_itr=0,
                         inner_batch_size=1000)
