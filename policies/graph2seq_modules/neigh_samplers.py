cd6b76a00e1eada97578db67a361ce15cfac481e
Erfan Mhp <thisiserfun@gmail.com>
2025-08-01 20:25:44 +0330

    fix bug

--- policies/graph2seq_modules/neigh_samplers.py
+++ policies/graph2seq_modules/neigh_samplers.py
@@ -14,5 +14,11 @@ class UniformNeighborSampler(Layer):
         ids, num_samples = inputs
         adj_lists = tf.nn.embedding_lookup(self.adj_info, ids)
         adj_lists = tf.transpose(tf.transpose(adj_lists))
-        adj_lists = tf.slice(adj_lists, [0,0], [-1, num_samples])
+        
+        # Get the actual width of adj_lists to avoid slice errors
+        adj_width = tf.shape(adj_lists)[1]
+        # Use minimum of requested samples and actual available columns
+        actual_samples = tf.minimum(num_samples, adj_width)
+        
+        adj_lists = tf.slice(adj_lists, [0,0], [-1, actual_samples])
         return adj_lists
