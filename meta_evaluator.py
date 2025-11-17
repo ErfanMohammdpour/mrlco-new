404c32c53677be2cbb98361752bd0f66e5045611
ErfanMhp <thisiserfun@gmail.com>
2025-11-17 23:57:48 +0330

    fixed

--- meta_evaluator.py
+++ meta_evaluator.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
                                 batch_size=100,
                                 graph_number=100,
                                 graph_file_paths=[
-                                    "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_16/random.20."
+                                    "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_12/random.20."
                                     ],
                                 time_major=False)
 
